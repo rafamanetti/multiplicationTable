0edc02040c935f163aed322c235abbcd23673d11
Rafael Braga Manetti <84095571+rafamanetti@users.noreply.github.com>
2024-07-22 14:38:26 -0300

    Added HTML5, CSS3, JavaScript File
    Added Model.html
    Added style.css
    Added script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function multitable() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('[ERROR] Type a Number!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+   
+}
